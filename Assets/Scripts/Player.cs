b874e43b3ff0f7c1bd0c127863b140622c4d6a63
unknown <>
2016-10-02 21:44:00 +0900

    ダッシュ実装

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField]
+    private bool tach;
+    [SerializeField]
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tach = Input.GetMouseButtonDown(0);
+        if (tach)
+        {
+            if (transform.localRotation.y == 90)
+                transform.localRotation = Quaternion.Euler(0, -90, 0);
+            else if (transform.localRotation.y == -90)
+                transform.localRotation = Quaternion.Euler(0, 90, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/Wall.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wall : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject player;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("hit");
+        }
+    }
+}
